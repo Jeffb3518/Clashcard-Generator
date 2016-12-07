6945beda65c9e39584e99ea777fa4f21fc5e0a77
Jeffrey Bauer <Jeffrey@Jeffreys-Air.attlocal.net>
2016-12-07 18:09:39 -0500

    Updates

--- basicflashcard.js
+++ basicflashcard.js
@@ -1,4 +1,19 @@
-var basic = function(front, back){
+var Basic = function(){
     this.front = front;
     this.back = back;
 }
+
+var question1 = new Basic()
+
+question1.front = "What is the Capital of Mississippi?";
+question1.back = "Jackson";
+
+var question2 = new Basic()
+
+question2.front = "What is the Capital of Kansas?";
+question2.back = "Topeka";
+
+var question3 = new Basic()
+
+question3.front = "What is the Capital of Idaho?"
+question3.back = "Boise"
--- clozeflashcard.js
+++ clozeflashcard.js
@@ -1,4 +1,19 @@
-var close = function(front, back){
+var Cloze = function(){
     this.front = front;
     this.back = back;
 }
+
+var question1 = new Cloze()
+
+question1.front = "Columbus?";
+question1.back = "is the Capital of Ohio";
+
+var question2 = new Cloze()
+
+question2.front = "Atlanta";
+question2.back = "is the Capital of Georgia";
+
+var question3 = new Cloze()
+
+question3.front = "Nashville"
+question3.back = "is the capital of Tennessee"
